9959c039e07d3320c7fa082046b54113ac9f5845
Seb-Skaitch <sebskinner@gmail.com>
2026-03-04 15:10:02 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,18 +7,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 )
-```
-
-4. Commit changes
-
-**Then update index.html**
-
-1. Click on `index.html` in your file list
-2. Click the pencil icon (Edit)
-3. Change this line:
-```
-<script type="module" src="/src/App.jsx"></script>
-```
-To this:
-```
-<script type="module" src="/src/main.jsx"></script>
